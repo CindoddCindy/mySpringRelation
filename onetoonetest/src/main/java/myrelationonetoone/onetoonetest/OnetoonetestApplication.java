2b0dea9f9a116810f9f7050adf7fe7568117ce04
CindoddCindy <cindodcindy@gmail.com>
2020-12-27 21:22:44 +0700

    tambah folder spring

---
+++ onetoonetest/src/main/java/myrelationonetoone/onetoonetest/OnetoonetestApplication.java
@@ -0,0 +1,13 @@
+package myrelationonetoone.onetoonetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnetoonetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnetoonetestApplication.class, args);
+	}
+
+}
---
+++ onetoonetest/src/test/java/myrelationonetoone/onetoonetest/OnetoonetestApplicationTests.java
@@ -0,0 +1,13 @@
+package myrelationonetoone.onetoonetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnetoonetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
