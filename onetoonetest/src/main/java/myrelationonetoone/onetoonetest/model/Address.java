7a9104fdf78e893b1ca5ea352e57223e25d8bc53
CindoddCindy <cindodcindy@gmail.com>
2020-12-27 22:03:43 +0700

    cuma tambah kelas address

--- onetoonetest/src/main/java/myrelationonetoone/onetoonetest/model/Address.java
+++ onetoonetest/src/main/java/myrelationonetoone/onetoonetest/model/Address.java
@@ -1,3 +1,28 @@
 package myrelationonetoone.onetoonetest.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="address")
+@Getter
+@Setter
 public class Address {
+    @Id
+    @Column(name="id")
+    @SequenceGenerator(initialValue=1, name="address_seq", sequenceName="address_sequence")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="address_seq")
+    private int id;
+
+    @Column(name="doornumber")
+    private String doorNumber;
+
+    @Column(name="street")
+    private String street;
+
+    @Column(name="city")
+    private String city;
 }
